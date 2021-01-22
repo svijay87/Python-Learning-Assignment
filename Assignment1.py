4d27843d24628302903f4a4a88db29910687c749
svijay87 <vijay.sachin23@gmail.com>
2021-01-22 13:32:34 +0530

    Update Assignment1.py
    Updated code

--- Assignment1.py
+++ Assignment1.py
@@ -1,26 +1,18 @@
-
-
 from abc import ABC, abstractmethod
 import requests
 import json
-import unittest
-
 
 class Universe(ABC):
-
     @abstractmethod
     def firewall(self):
         pass
 
-class NASA(Universe):
+class Nasa(Universe):
     def __init__(self, from_date : str, to_date : str) :
-        self.cname = 'NASA'
         self.from_date = from_date
         self.to_date = to_date
    
     def firewall(self) -> bool:
-        print('This is the implementation')
-       
         url = f"https://ssd-api.jpl.nasa.gov/fireball.api?date-min={self.from_date}&date-max={self.to_date}&req-alt=true&sort=-energy"
        
         res = requests.get(url)
@@ -28,9 +20,10 @@ class NASA(Universe):
         res.raise_for_status
        
         print(res.content)
+        result = json.loads(res.content)
         return True
        
-class ISRO(NASA):
+class Isro(Nasa):
     def __init__(self, from_date : str, to_date : str):
         print('INIT ISRO')
         print(from_date)
@@ -42,14 +35,14 @@ class ISRO(NASA):
         super().fireball()
 
 
-obj_nasa = NASA('2017-01-01','2020-01-01')
+obj_nasa = Nasa('2017-01-01','2020-01-01')
 obj_nasa.firewall()
 
 try:
-    isro_obj = ISRO('2017-01-01','2020-01-01')
-    #print(isro_obj.firewall())
-except TypeError as te:
-    print(f'Type error occured while creating obj of ISRO :: {te}')
+    isro_obj = Isro('2017-01-01','2020-01-01')
+    isro_obj.firewall()
+except Exception as e:
+    print(f'Type error occured while creating obj of ISRO :: {e}')
 
 print('Done')
 
